107caa46628bcf20385f5f101e8df7a6822e9828
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2020-02-01 16:50:30 -0700

    data cleaning and vizualization

--- src/cleaning.py
+++ src/cleaning.py
@@ -9,5 +9,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, roc_curve, auc
 
 if __name__=='__main__':
-    turnover = pd.read_csv("turnover.csv")
+    turnover = pd.read_csv("../data/turnover.csv")
     turnover["salary"] = turnover["salary"].astype('category').cat.reorder_categories(['low', 'medium', 'high']).cat.codes
+    turnover.rename(columns={'sales': 'department'}, inplace=True)
+    department = pd.get_dummies(turnover["department"])
+    turnover = turnover.drop(["department"], axis=1)
+
+    # plotting the correlation matrix
+    # as seaborn is based on matplotlib, we need to use plt.show() to see the plot
+    sns.heatmap(turnover.corr())
+    plt.show()
+    plt.savefig('correlation_matrix.png')
+    plt.tight_layout()
+
+    turnover = turnover.join(department)
+
+    # the percentage of leavers
+    turnover['left'].value_counts()/len(turnover)*100
