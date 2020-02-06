8d98f438910dc68f2773c5f264b169ba145e013d
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2020-02-06 09:00:50 -0700

    im done with the plts

--- src/employee_turnover.py
+++ src/employee_turnover.py
@@ -8,99 +8,180 @@ from sklearn import tree
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 
+def plot_histograms():
 
-# cleaning data 
-turnover = pd.read_csv("../data/turnover.csv")
-salary = turnover['salary'].value_counts()
-salary_col = list(salary.index)
-turnover["salary"] = turnover["salary"].astype('category').cat.reorder_categories(salary_col).cat.codes
-salary_code = turnover.salary.value_counts()
-salary_code_col = list(salary_code.index)
+    continous_features = ['satisfaction_level_percentage','last_evaluation_percentage','average_montly_hours']
+    
+    for feat in continous_features:
+        fig, ax = plt.subplots(figsize=(8,5))
+        ax.hist(x=stayed_df[feat], stacked=True, label='stayed', alpha=0.8)
+        ax.hist(x=left_df[feat], stacked=True, label='left', alpha=0.8)
+        ax.set_title(f'{feat}')
+        ax.set_xlabel(f'{feat}')
+        ax.set_ylabel('Frequency Count')
+        plt.legend(loc='best')
+        fig.tight_layout(pad=2)
+        plt.savefig(f'{feat}.png')
 
-salary_dict = {'salary': salary_col, 'code': salary_code_col}
-salary_df = pd.DataFrame(data=salary_dict) # use for eda
+def create_cat_percentage_df():
 
-rename_columns = {'satisfaction_level': 'satisfaction_level_percentage',\
-                  'last_evaluation': 'last_evaluation_percentage',\
-                  'time_spend_company': 'time_spend_company_years',\
-                  'sales': 'department'}
-turnover.rename(columns=rename_columns, inplace=True)
+    left_df = turnover[turnover['left']==1]
+    stayed_df = turnover[turnover['left']==0]
 
-department = turnover.department.value_counts()
-department_col = list(department.index)
-turnover["department"] = turnover["department"].astype('category').cat.reorder_categories(department_col).cat.codes
-# department = pd.get_dummies(turnover["department"])
-# turnover = turnover.drop(["department"], axis=1)
-department_code = turnover.department.value_counts()
-department_code_col = list(department_code.index)
+    cat_feature = ['number_project', 'time_spend_company_years',\
+                   'Work_accident', 'promotion_last_5years', 'department', 'salary']
 
-department_dict = {'department': department_col, 'code': department_code_col}
-department_df = pd.DataFrame(data=department_dict) # use for eda
+    for cat in cat_feature:
+        left_df = left_df[cat_feature]
+        stayed_df = stayed_df[cat_feature]
 
+        # initiating the dataframe
+        current = turnover[cat].value_counts()
+        current_col = list(current.index)
+        current_code = turnover[cat].value_counts()
+        current_code_col = list(current_code.index)
 
-# Data Visualization
+        current_dict = {f'{cat}': current_col, 'code': current_code_col} # data for new dataframe
+        current_df = pd.DataFrame(data=current_dict)
 
-## department eda deepdive
-left_df = turnover[turnover['left']==1]
-department_df['left'] = left_df['department'].value_counts()
+        # adding percentage convergance
+        current_df['left'] = left_df[cat].value_counts()
+        current_df['stayed'] = stayed_df[cat].value_counts()
+        current_df['total_count'] = turnover[cat].value_counts()
+        current_df['left_percentage'] = current_df['left'] / current_df['total_count']
+        current_df.sort_values(by='left_percentage', axis=0, inplace=True)
 
-stayed_df = turnover[turnover['left']==0]
-department_df['stayed'] = stayed_df['department'].value_counts()
-
-department_df['total_count'] = turnover['department'].value_counts()
-department_df['left_percentage'] = department_df['left'] / department_df['total_count']
-department_df.sort_values(by='left_percentage', axis=0, inplace=True)
-
-labels = department_df['department']
-data = department_df['left_percentage']
-N = 10
-fig, ax = plt.subplots(figsize=(8,5))
-width = 0.8
-tickLocations = np.arange(N)
-ax.bar(tickLocations, data, width, linewidth=3.0, align='center')
-ax.set_xticks(ticks=tickLocations)
-ax.set_xticklabels(labels)
-ax.set_xlim(min(tickLocations)-0.6,\
-            max(tickLocations)+0.6)
-ax.set_xlabel('Department')
-ax.set_ylabel('Employee Percent Turnvover')
-ax.set_yticks(np.linspace(0,0.5,6))
-ax.yaxis.grid(True)
-ax.set_title('Employer Turnover by Department')
-fig.tight_layout(pad=1)
-plt.savefig('Employer_Turnover_by_Department.png')
+        plot_side_by_side_percentage_barcharts(current_df)
 
 
 
-## Salary eda deepdive
 
-left_df = turnover[turnover['left']==1]
-salary_df['left'] = left_df['salary'].value_counts()
-
-stayed_df = turnover[turnover['left']==0]
-salary_df['stayed'] = stayed_df['salary'].value_counts()
-
-salary_df['total_count'] = turnover['salary'].value_counts()
-salary_df['left_percentage'] = salary_df['left'] / salary_df['total_count']
-salary_df.sort_values(by='left_percentage', axis=0, inplace=True)
-
-labels = salary_df['salary']
-data = salary_df['left_percentage']
-N = 3
-fig, ax = plt.subplots(figsize=(8,5))
-width = 0.8
-tickLocations = np.arange(N)
-ax.bar(tickLocations, data, width, linewidth=3.0, align='center')
-ax.set_xticks(ticks=tickLocations)
-ax.set_xticklabels(labels)
-ax.set_xlim(min(tickLocations)-0.6,\
-            max(tickLocations)+0.6)
-ax.set_xlabel('Salary Ranking')
-ax.set_ylabel('Employee Percent Turnvover')
-ax.set_yticks(np.linspace(0,0.5,6))
-ax.yaxis.grid(True)
-ax.set_title('Employer Turnover by Salary Rank')
-fig.tight_layout(pad=1)
-plt.savefig('Employer_Turnover_by_Salary_rank.png')
+def plot_side_by_side_percentage_barcharts(df):
+ 
+    column = df.columns[0]
+    labels = df[column]
+    data = df['left_percentage']
+    N = len(df)
+    fig, ax = plt.subplots(figsize=(8,5))
+    width = 0.8
+    tickLocations = np.arange(N)
+    ax.bar(tickLocations, data, width, linewidth=3.0, align='center')
+    ax.set_xticks(ticks=tickLocations)
+    ax.set_xticklabels(labels)
+    ax.set_xlim(min(tickLocations)-0.6,\
+                max(tickLocations)+0.6)
+    ax.set_xlabel(f'{column}')
+    ax.set_ylabel('Employee Percent Turnvover')
+    # ax.set_yticks(np.linspace(0,max(),6))
+    ax.yaxis.grid(True)
+    ax.set_title(f'Employer Turnover by {column}')
+    fig.tight_layout(pad=1)
+    plt.savefig(f'Employer_Turnover_by_{column}.png')
 
 
+if __name__=='__main__':
+    # cleaning data 
+    turnover = pd.read_csv("../data/turnover.csv")
+    salary = turnover['salary'].value_counts()
+    salary_col = list(salary.index)
+    turnover["salary"] = turnover["salary"].astype('category').cat.reorder_categories(salary_col).cat.codes
+    salary_code = turnover.salary.value_counts()
+    salary_code_col = list(salary_code.index)
+
+    salary_dict = {'salary': salary_col, 'code': salary_code_col}
+    salary_df = pd.DataFrame(data=salary_dict) # use for eda
+
+    rename_columns = {'satisfaction_level': 'satisfaction_level_percentage',\
+                    'last_evaluation': 'last_evaluation_percentage',\
+                    'time_spend_company': 'time_spend_company_years',\
+                    'sales': 'department'}
+    turnover.rename(columns=rename_columns, inplace=True)
+
+    department = turnover.department.value_counts()
+    department_col = list(department.index)
+    turnover["department"] = turnover["department"].astype('category').cat.reorder_categories(department_col).cat.codes
+    # department = pd.get_dummies(turnover["department"])
+    # turnover = turnover.drop(["department"], axis=1)
+    department_code = turnover.department.value_counts()
+    department_code_col = list(department_code.index)
+
+    department_dict = {'department': department_col, 'code': department_code_col}
+    department_df = pd.DataFrame(data=department_dict) # use for eda
+
+    create_cat_percentage_df()
+    # plt.show()
+
+    # plotting the correlation matrix
+    # as seaborn is based on matplotlib, we need to use plt.show() to see the plot
+    fig, ax = plt.subplots(figsize=(9,6))
+    ax = sns.heatmap(turnover.corr())
+    # plt.show()
+    # plt.savefig('correlation_matrix.png')
+    plt.tight_layout(pad=4)
+
+    # Data Visualization
+
+    ## department eda deepdive
+    left_df = turnover[turnover['left']==1]
+    department_df['left'] = left_df['department'].value_counts()
+
+    stayed_df = turnover[turnover['left']==0]
+    department_df['stayed'] = stayed_df['department'].value_counts()
+
+    department_df['total_count'] = turnover['department'].value_counts()
+    department_df['left_percentage'] = department_df['left'] / department_df['total_count']
+    department_df.sort_values(by='left_percentage', axis=0, inplace=True)
+
+    labels = department_df['department']
+    data = department_df['left_percentage']
+    N = 10
+    fig, ax = plt.subplots(figsize=(8,5))
+    width = 0.8
+    tickLocations = np.arange(N)
+    ax.bar(tickLocations, data, width, linewidth=3.0, align='center')
+    ax.set_xticks(ticks=tickLocations)
+    ax.set_xticklabels(labels)
+    ax.set_xlim(min(tickLocations)-0.6,\
+                max(tickLocations)+0.6)
+    ax.set_xlabel('Department')
+    ax.set_ylabel('Employee Percent Turnvover')
+    ax.set_yticks(np.linspace(0,0.5,6))
+    ax.yaxis.grid(True)
+    ax.set_title('Employer Turnover by Department')
+    fig.tight_layout(pad=1)
+    plt.savefig('Employer_Turnover_by_Department.png')
+
+    ## Salary eda deepdive
+    left_df = turnover[turnover['left']==1]
+    salary_df['left'] = left_df['salary'].value_counts()
+
+    stayed_df = turnover[turnover['left']==0]
+    salary_df['stayed'] = stayed_df['salary'].value_counts()
+
+    salary_df['total_count'] = turnover['salary'].value_counts()
+    salary_df['left_percentage'] = salary_df['left'] / salary_df['total_count']
+    salary_df.sort_values(by='left_percentage', axis=0, inplace=True)
+
+    labels = salary_df['salary']
+    data = salary_df['left_percentage']
+    N = 3
+    fig, ax = plt.subplots(figsize=(8,5))
+    width = 0.8
+    tickLocations = np.arange(N)
+    ax.bar(tickLocations, data, width, linewidth=3.0, align='center')
+    ax.set_xticks(ticks=tickLocations)
+    ax.set_xticklabels(labels)
+    ax.set_xlim(min(tickLocations)-0.6,\
+                max(tickLocations)+0.6)
+    ax.set_xlabel('Salary Ranking')
+    ax.set_ylabel('Employee Percent Turnvover')
+    ax.set_yticks(np.linspace(0,0.5,6))
+    ax.yaxis.grid(True)
+    ax.set_title('Employer Turnover by Salary Rank')
+    fig.tight_layout(pad=1)
+    plt.savefig('Employer_Turnover_by_Salary_rank.png')
+
+
+    ## continous-histograms
+    plot_histograms()
+    # plt.show()
